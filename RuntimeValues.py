f204293a83014701c79cdfe627432c320ba79ce6
gbk546 <geoff.klassen@usask.ca>
2025-03-06 19:46:46 -0600

    Resetting test_lower_bound to 1 and show_images to False

--- RuntimeValues.py
+++ RuntimeValues.py
@@ -53,7 +53,7 @@ save_downloaded_images = True  # If True, images downloaded at runtime are saved
 user = "geoff.klassen@usask.ca"
 passw = "Phi1*618ah"
 
-show_images = True  # If True, images of the screenshots will be shown during runtime (mostly for debugging).
+show_images = False  # If True, images of the screenshots will be shown during runtime (mostly for debugging).
 
 app_area_scale_factor = 1  # In addition to the screenshot_scale_factor, this is how much to scale the cropped image
                               # when searching for app-level data
@@ -63,7 +63,7 @@ ERR_CONFIDENCE = f"ERR Values below {int(conf_limit)}% confidence"
 # Location of PyTesseract on local drive
 # pytesseract.pytesseract.tesseract_cmd = r'C:\Users\gbk546\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'  # At U of S
 
-test_lower_bound = 3362
+test_lower_bound = 1
 test_upper_bound = 19313
 # 553 for HappyB2.0 (pre-launch)
 # 19313 URLs for HappyB2.0 2024 Aug-Dec
